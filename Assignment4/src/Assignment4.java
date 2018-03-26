2c1515dea469cc0a1935f163d2d1b7ccab0f3f9f
anorangesky <agnes.petajavaara@gmail.com>
2018-03-26 21:49:51 +0200

    this is the first commit

---
+++ Assignment4/src/Assignment4.java
@@ -0,0 +1,13 @@
+public class Assignment4 {
+    //This is just a template
+    public static String main(String args){
+        int testNumber = 1234567;
+
+        for(int i = 0; i < testNumber; i ++){
+            i += 10;
+        }
+        String done = "done";
+        return done;
+
+    }
+}
