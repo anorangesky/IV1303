a540e7e4ba6ee5fd1aba5de4e7af9fe47298d0e3
anorangesky <agnes.petajavaara@gmail.com>
2018-03-26 22:17:07 +0200

    added fibonacci of 10

--- Assignment4/src/Assignment4.java
+++ Assignment4/src/Assignment4.java
@@ -1,13 +1,20 @@
+import java.io.*;
+
 public class Assignment4 {
-    //This is just a template
-    public static String main(String args){
-        int testNumber = 1234567;
 
-        for(int i = 0; i < testNumber; i ++){
-            i += 10;
+    //This is just a test template
+    public static class Fib{
+        public static int fib(int n){
+            if(n <= 1) return n;
+            return fib(n-1) + fib(n-2);
         }
-        String done = "done";
-        return done;
-
+    }
+    public static void main(String[] args) {
+        //int n = Integer.parseInt(args[0]);
+        int n = 10;
+        int i;
+        for (i = 0; i < n; i++){
+            System.out.println(i+1 + ": " + Fib.fib(i));
+        }
     }
 }
