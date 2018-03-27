02166399d8183d6706388d749c61eb9ff7784436
anorangesky <agnes.petajavaara@gmail.com>
2018-03-27 23:34:45 +0200

    added 'fail' as result of test

--- Assignment4/src/FibonacciTest.java
+++ Assignment4/src/FibonacciTest.java
@@ -1,9 +1,11 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class FibonacciTest {
     @Test
     public void fibOfTenSHouldReturn34() throws Exception{
-
+        fail();
     }
 
 }
