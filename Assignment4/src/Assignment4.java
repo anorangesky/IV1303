6c16dd0e7691143d1f937f53ecfa43c018cec3e7
vcvlk <vlk.vendela@gmail.com>
2018-03-27 16:41:04 +0200

    added blank space 10/10

--- Assignment4/src/Assignment4.java
+++ Assignment4/src/Assignment4.java
@@ -2,6 +2,7 @@ public class Assignment4 {
 
     //This is just a test template
     public static class Fib {
+        
         public static int fib(int n) {
             if (n <= 1) return n;
             return fib(n - 1) + fib(n - 2);
