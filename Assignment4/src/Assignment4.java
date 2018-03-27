3ed93c65cafa0cead1562fb69b5482aedccbdb5f
anorangesky <agnes.petajavaara@gmail.com>
2018-03-27 09:05:05 +0200

    Added main class

--- Assignment4/src/Assignment4.java
+++ Assignment4/src/Assignment4.java
@@ -1,20 +1,10 @@
-import java.io.*;
-
 public class Assignment4 {
 
     //This is just a test template
-    public static class Fib{
-        public static int fib(int n){
-            if(n <= 1) return n;
-            return fib(n-1) + fib(n-2);
-        }
-    }
-    public static void main(String[] args) {
-        //int n = Integer.parseInt(args[0]);
-        int n = 10;
-        int i;
-        for (i = 0; i < n; i++){
-            System.out.println(i+1 + ": " + Fib.fib(i));
+    public static class Fib {
+        public static int fib(int n) {
+            if (n <= 1) return n;
+            return fib(n - 1) + fib(n - 2);
         }
     }
 }
---
+++ Assignment4/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        //int n = Integer.parseInt(args[0]);
+        int n = 10;
+        int i;
+        for (i = 0; i < n; i++) {
+            System.out.println(i + 1 + ": " + Assignment4.Fib.fib(i));
+        }
+    }
+}
