cdd0f86738744ae8cd49c182f367ebd9d8468b29
anorangesky <agnes.petajavaara@gmail.com>
2018-03-27 22:54:12 +0200

    added message to vcvlk

--- Assignment4/src/Main.java
+++ Assignment4/src/Main.java
@@ -4,6 +4,8 @@ public class Main {
         int i;
         for (i = 0; i < n; i++) {
             System.out.println(i + 1 + ": " + Assignment4.Fib.fib(i));
+            System.out.println("HEJ VLK!!!!");
         }
     }
 }
+
