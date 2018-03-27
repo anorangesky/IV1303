b38363d6d4513348035251b266cd9c9f1a6061dc
anorangesky <agnes.petajavaara@gmail.com>
2018-03-27 09:31:12 +0200

    added test class

--- Assignment4/src/Main.java
+++ Assignment4/src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        //int n = Integer.parseInt(args[0]);
-        int n = 10;
+        int n = Integer.parseInt(args[0]);
         int i;
         for (i = 0; i < n; i++) {
             System.out.println(i + 1 + ": " + Assignment4.Fib.fib(i));
