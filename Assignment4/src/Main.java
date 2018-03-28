83522b7e34feda88d0d7e6c1e2fe1a5070cd4e3d
anorangesky <agnes.petajavaara@gmail.com>
2018-03-28 21:43:02 +0200

    Removed old code and fixed some errors

--- Assignment4/src/Main.java
+++ Assignment4/src/Main.java
@@ -1,17 +1,11 @@
+
 public class Main {
     public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]);
-        int i;
-        for (i = 0; i < n; i++) {
-            System.out.println(i + 1 + ": " + Assignment4.Fib.fib(i));
-
-            System.out.println("HEJ VLK!!!!");
-            System.out.println("Isn't Fibonacci the best calculator ever? And so are you, so what is " + n + "divided by 3?");
-        }
-
-        System.out.println(simpleLogicCalc(args));
-
-
+        int int1 = Integer.parseInt(args[0]);
+        int int2 = Integer.parseInt(args[2]);
+        //char op = ??
+       //how do i read the "char" from the args[1]?
+        System.out.println(Assignment4.simpleLogicCalc(int1, op, int2));
     }
 }
 
